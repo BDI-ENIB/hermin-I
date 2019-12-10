846fc633651794c2dacf822c1056b66e4277330e
HeeroYuy99 <45212520+HeeroYuy99@users.noreply.github.com>
2019-12-10 20:38:55 +0100

    Add files via upload

---
+++ sequenceur.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+int time = 10000;//time befor opening parachute case
+
+int jackPin = 2;
+int ledPin = 3;
+int motorPin = 9;
+int buzzerPin = 6;
+
+Servo myServo;
+
+void setup()
+{
+  //init
+  pinMode (jackPin, INPUT);
+  pinMode (ledPin, OUTPUT);
+  pinMode (buzzerPin, OUTPUT);
+  
+  myServo.attach(motorPin);
+  myServo.write(0);
+  
+  
+  while (digitalRead(jackPin)==1)//until removing jack
+  {
+    //waiting
+  }
+  digitalWrite(ledPin, HIGH);//turning n the led
+  
+  delay(time);//waiting until end of flight
+  myServo.write(90);//realaising parachute
+  analogWrite(buzzerPin, 90);
+}
+void loop()
+{
+  
+  
+}

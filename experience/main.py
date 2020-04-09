23e0a8c42a4560c85a568f6bf46fb72169c69462
AntoineGuillemaud <antoine.guillemaud1@gmail.com>
2020-04-09 19:06:03 +0200

    implémentation des 2 thermistances et des 2 capteurs de pressions

--- experience/main.py
+++ experience/main.py
@@ -24,6 +24,8 @@ def writeFile(data,file):
     return
 
 #INITIALISATION
+pycom.heartbeat(False)
+liste_capteur = {} #dictionnaire qui stoque la fréquence d'enregistrement et la marche à suivre pour récuperer et enregistrer les données de chaque capteurs
 #usb
 uart_pc = machine.UART(0, 115200)
 os.dupterm(uart_pc)
@@ -31,17 +33,66 @@ os.dupterm(uart_pc)
 nombreLigneParFichier = 100
 #gps
 uart_gps = UART(1,9600)
-pycom.heartbeat(False)
 envoyerDonneeGPS=0
-frequence_cycle_GPS = 2 #le GPS est enregistré tous les "n" cycles
+def fonction_gps():
+    gps_byte=uart_gps.readline()
+    if envoyerDonneeGPS<=5:
+        uart_gps.write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
+        envoyerDonneeGPS=envoyerDonneeGPS+1
+    if 5<envoyerDonneeGPS and envoyerDonneeGPS<10:
+        uart_gps.write(b'$PMTK220,500*2B\r\n')
+        envoyerDonneeGPS=envoyerDonneeGPS+1
+    gps_str=str(gps_byte)
+    print(gps_str)
+    splited_gps_str=split(gps_str)
+    if ("A"  in splited_gps_str) :
+        pycom.rgbled(0x00FF00)
+    writeFile(str(gps_str),file)
+liste_capteur["gps"] = {"frequence":2,"fonction":fonction_gps} # ["frequence":n] => le GPS est enregistré tous les "n" cycles
 #mpu
 i2c = I2C(0)
 sensor = MPU9250(i2c) #m/s^2, rad/s and uT
-frequence_cycle_MPU = 1
-#thermistance
-adc = machine.ADC(0)
-adc_c = adc.channel(pin='P13',attn=machine.ADC.ATTN_0DB) #pour une pile de 0.629V , print(adc_c.value()) donne 2070
-frequence_cycle_thermi = 1
+def fonction_mpu():
+    mpu_str = str('acc'+str(sensor.acceleration)+'gyro'+str(sensor.gyro)+'mag'+str(sensor.magnetic))
+    print(mpu_str)
+    writeFile(mpu_str,file)
+liste_capteur["mpu"] = {"frequence":1,"fonction":fonction_mpu}
+#thermistance 1
+adc_1 = machine.ADC(0)
+adc_c1 = adc.channel(pin='P13',attn=machine.ADC.ATTN_0DB) #pour une pile de 0.629V , print(adc_c.value()) donne 2070
+def fonction_thermi_1():
+    adc_c1()
+    tension_thermi_1 = adc_c1.value()
+    print(tension_thermi_1)
+    writeFile(str(tension_thermi_1),file)
+liste_capteur["thermi_1"] = {"frequence":1,"fonction":fonction_thermi_1}
+#thermistance 2
+adc_2 = machine.ADC(0)
+adc_c2 = adc.channel(pin='P16',attn=machine.ADC.ATTN_0DB) #pour une pile de 0.629V , print(adc_c.value()) donne 2070
+def fonction_thermi_2():
+    adc_c2()
+    tension_thermi_2 = adc_c2.value()
+    print(tension_thermi_2)
+    writeFile(str(tension_thermi_2),file)
+liste_capteur["thermi_2"] = {"frequence":1,"fonction":fonction_thermi_2}
+#capteur pression 1
+adc_3 = machine.ADC(2)
+adc_c3 = adc.channel(pin='P14',attn=machine.ADC.ATTN_0DB) #pour une pile de 0.629V , print(adc_c.value()) donne 2070
+def fonction_pression_1():
+    adc_c3()
+    tension_pression_1 = adc_c3.value()
+    print(tension_pression_1)
+    writeFile(str(tension_pression_1),file)
+liste_capteur["pression_1"] = {"frequence":1,"fonction":fonction_pression_1}
+#capteur pression 2
+adc_4 = machine.ADC(2)
+adc_c4 = adc.channel(pin='P17',attn=machine.ADC.ATTN_0DB) #pour une pile de 0.629V , print(adc_c.value()) donne 2070
+def fonction_pression_1():
+    adc_c4()
+    tension_pression_2 = adc_c4.value()
+    print(tension_pression_2)
+    writeFile(str(tension_pression_2),file)
+liste_capteur["pression_2"] = {"frequence":1,"fonction":fonction_pression_2}
 
 numeroFichier=0
 numero_ligne=0
@@ -67,31 +118,9 @@ while True:
     start_time = time.ticks_us()
     writeFile(str(start_time),file)
 
-    if numero_cycle % frequence_cycle_GPS == 0:
-        gps_byte=uart_gps.readline()
-        if envoyerDonneeGPS<=5:
-            uart_gps.write(b'$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
-            envoyerDonneeGPS=envoyerDonneeGPS+1
-        if 5<envoyerDonneeGPS and envoyerDonneeGPS<10:
-            uart_gps.write(b'$PMTK220,500*2B\r\n')
-            envoyerDonneeGPS=envoyerDonneeGPS+1
-        gps_str=str(gps_byte)
-        print(gps_str)
-        splited_gps_str=split(gps_str)
-        if ("A"  in splited_gps_str) :
-            pycom.rgbled(0x00FF00)
-        writeFile(str(gps_str),file)
-
-    if numero_cycle % frequence_cycle_MPU == 0:
-        mpu_str = str('acc'+str(sensor.acceleration)+'gyro'+str(sensor.gyro)+'mag'+str(sensor.magnetic))
-        print(mpu_str)
-        writeFile(mpu_str,file)
-
-    if numero_cycle % frequence_cycle_thermi ==0:
-        adc_c()
-        tension_thermi = adc_c.value()
-        print(tension_thermi)
-        writeFile(str(tension_thermi),file)
+    for capteur in liste_capteur:
+        if numero_cycle % capteur["frequence"] ==0:
+            capteur["fonction"]()
 
     numero_cycle +=1
     numero_ligne=numero_ligne+1

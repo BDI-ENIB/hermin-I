3a18d58c84ef48554c0f7f5ae3bab82adf2c323e
a7nougie <axel.nougier@gmail.com>
2020-05-22 16:58:54 +0200

    log support

--- dolmen/python/Widgets.py
+++ dolmen/python/Widgets.py
@@ -1,60 +1,157 @@
-from tkinter import *
+#from tkinter import *
+import tkinter as tk
 import Windows
+from PIL import Image, ImageTk
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import time
+
+def addImage(windows,myphoto,colorFont,row,column):
+
+    photo = tk.PhotoImage(file=myphoto,master=windows)
+    canvas = tk.Canvas(windows,width=photo.width(), height=photo.height(),bg = 'white')
+    canvas.create_image(0, 0, anchor="nw", image=photo)
+    canvas.grid(row=row, column=column)
+    canvas.image=photo
+    windows.mainloop()
+
 
 class Widgets():
-    def __init__(self,windows, title,position):
+
+    def __init__(self,windows, title,colorFont,colorText,row,column):
+        
         self.windows=windows
         self.title=title
-        self.position=position
+        self.colorFont=colorFont
+        self.colorText=colorText
+        self.row=row
+        self.column=column
+
 
 class ButtonDisplay(Widgets):
-    def __init__(self,windows, title,position,color,function,x,y):
-        Widgets.__init__(self,windows,title,position)
-        self.color=color
-        
-        self.function=function
+
+    def __init__(self,windows, title,colorFont,colorText,colorSelect,function,x,y,row,column):
+
+        Widgets.__init__(self,windows,title,colorFont,colorText,row,column)              
         self.x=x
         self.y=y
-        
-        self.windows.button= Button(self.windows, text=self.title, fg=self.color,command=self.function)
-        self.windows.button.pack(side=self.position)
-        self.windows.button.config( width = self.x, height = self.y )
+        self.windows = windows
+        self.function=function 
+        self.colorSelect=colorSelect      
+        self.button= tk.Button(self.windows,anchor="center",text=self.title, bg=self.colorFont,fg=self.colorText,activebackground=self.colorSelect,command=self.function)
+        self.button.grid(row=self.row, column=self.column)   
+        self.button.config( width = self.x, height = self.y )
+
+
+    def enable(self):
+        self.button.config(state=tk.NORMAL)
+
+
+    def disable(self):
+        self.button.config(state=tk.DISABLED)
+    
+    def getState(self):
+        return self.button['state']
+
 
 class TextInput(Widgets):
-    def __init__(self,windows, title, position):
-        Widgets.__init__(self,windows,title,position)
-        self.entry=StringVar()
-        if(self.title!=None):
-            self.windows.message = Label(self.windows, text=self.title)
-            self.windows.message.pack()
-        StringText = Entry(self.windows,textvariable=self.entry)
-        StringText.pack(side=self.position)
 
-    def getEntry():
-        if (self.entry):
-            return self.entry
+    def __init__(self,windows, title,colorFont,colorText,rowText,columnText,rowInput,columnInput):
+
+        Widgets.__init__(self,windows,title,colorFont,colorText,rowInput,columnInput)
+        self.entry=tk.StringVar()
+        self.default = "texte par défaut"
+        self.rowText=rowText
+        self.columnText=columnText
+        if(self.title!=None):
+            self.windows.message = tk.Label(self.windows, bg=self.colorFont,fg=self.colorText,text=self.title)
+            self.windows.message.grid(row=self.rowText, column=self.columnText)   
+        self.StringText = tk.Entry(self.windows,bg=self.colorFont,fg=self.colorText,textvariable=self.entry)
+        self.entry.set(self.default)
+        self.StringText.grid(row=self.row, column=self.column)   
+
+    def getEntry(self):
+
+        if (self.StringText):
+            data = str(self.StringText.get())
+            #print(data)
+            return data
 
 class Case(Widgets):
-    def __init__(self,windows, title, position, function):
-        Widgets.__init__(self,windows,title,position)
-        self.state=StringVar()
+
+    def __init__(self,windows, title,colorFont,colorText,colorSelect,function,row,column,default):
+
+        Widgets.__init__(self,windows,title,colorFont,colorText,row,column)
+        self.state=tk.StringVar()
         self.function=function
-        case = Checkbutton(self.windows, text=self.title, variable=self.state,command=self.function)
-        case.pack(side=self.position)
-    def getState():
-        return self.state
+        self.colorSelect=colorSelect
+        #self.default=0
+        self.default=default
+        self.case = tk.Checkbutton(self.windows, text=self.title, bg=self.colorFont,fg=self.colorText,activebackground=self.colorSelect,variable=self.state,command=self.function)
+        self.state.set(self.default)
+        self.case.grid(row=self.row, column=self.column) 
+
+
+    def getState(self):
+
+        print(self.state.get())
+        return self.state.get()
+
+
+    def enable(self):
+        self.button.config(state=tk.NORMAL)   
+        
+
+    def disable(self):
+        self.button.config(state=tk.DISABLED) 
 
 class DropdownList(Widgets):
-    def __init__(self,windows, title, position, dropdownList):
-        Widgets.__init__(self,windows,title,position)
+
+    def __init__(self,windows, title, dropdownList,colorFont,colorText,row,column):
+
+        Widgets.__init__(self,windows,title,colorFont,colorText,row,column)
         self.dropdownList=dropdownList
-        list = Listbox(self.windows)
+        self.default=1
+        self.listbox = tk.Listbox(self.windows, bg=self.colorFont,fg=colorText)
+
         for i in range(0,len(self.dropdownList)):
-            list.insert(END,self.dropdownList[i])
-        list.pack(side=self.position)
+            self.listbox.insert(tk.END,self.dropdownList[i])
+
+        self.listbox.activate(self.default)
+        self.listbox.selection_set( first = self.default )
+        self.listbox.grid(row=self.row, column=self.column)
+
+
+    def getChoose(self):
+
+        print(self.listbox.get(tk.ACTIVE))
+        return self.listbox.get(tk.ACTIVE)
+
 
 class TextToPrint(Widgets):
-    def __init__(self,windows, title, position):
-        Widgets.__init__(self,windows,title,position)
-        self.windows.message = Label(self.windows, text=self.title)
-        self.windows.message.pack(side=self.position) 
+
+    def __init__(self,windows, title,colorFont,colorText,row,column):
+
+        Widgets.__init__(self,windows,title,colorFont,colorText,row,column)
+        self.windows.message = tk.Label(self.windows, bg=self.colorFont, fg=self.colorText,text=self.title)
+        self.windows.message.grid(row=self.row, column=self.column) 
+
+
+class timeDisplay(Widgets):
+
+    def __init__(self,windows, title,colorFont,colorText,row,column):
+
+        Widgets.__init__(self,windows,title,colorFont,colorText,row,column)
+        self.windows.message = tk.Label(self.windows, bg=self.colorFont, fg=self.colorText,text="")        
+        self.windows.message.grid(row=self.row, column=self.column)
+        self.now = time.strftime("%H:%M:%S")
+
+
+    def update_clock(self):
+        self.now = time.strftime("%H:%M:%S")
+        self.windows.message.configure(text=self.now)       
+
+
+
--- dolmen/python/Windows.py
+++ dolmen/python/Windows.py
@@ -1,9 +1,110 @@
-from tkinter import *
-class Windows(Frame):
-    def __init__(self, windows,title, x,y,**kwargs):
-        Frame.__init__(self, windows, width=x, height=y, **kwargs)
-        self.pack(fill=BOTH)
-        windows.geometry(str(x) + "x" + str(y))
-        windows.title(title)
+#from tkinter import *
+import Config
+import logging
+import time
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import sys
+class Windows(tk.Frame):
+    def __init__(self,title, colorFont,x,y,function,ligne,colonne):
+        #tk.Tk.__init__(self, windows, width=x, height=y, bg=colorFont, **kwargs)
+        #self.pack_propagate(0)
+        self.windows=tk.Tk()
+        super().__init__(self.windows)        
+        self.color = colorFont
+        self.configure(bg=colorFont)
+        self.width=x
+        self.height=y   
+        self.pack(fill='both')
+        self.ws = self.windows.winfo_screenwidth()
+        self.hs = self.windows.winfo_screenheight()
+        self.widthCenter = (self.ws/2) - (self.width/2)
+        self.heightCenter = (self.hs/2) - (self.height/2)
+        self.windows.rowconfigure(ligne, weight=1)
+        self.windows.columnconfigure(colonne, weight=1)
+
+
+        if (self.width==0 and self.height==0):
+            self.windows.geometry(str(self.ws) + "x" + str(self.hs) )
+
+        else :
+            self.windows.geometry(str(self.width) + "x" + str(self.height) + "+" + str(int(self.widthCenter)) + "+" +str(int(self.heightCenter)))
+
+        self.windows.title(title)
+        self.function=lambda: function()
+        self.windows.protocol("WM_DELETE_WINDOW",self.on_closing )
+
+
+    def on_closing(self):
+            
+            if(self.function != None):
+                #self.windows.destroy()
+                self.function()
+            else :
+                self.windows.destroy()
+
+
+def exit():
+
+    if(messageAskyesno("Quit", "Do you want to quit?")):
+            logging.info(str(time.strftime('%Hh' '%M' ' %S')) + ' Exit Dolmen')
+            sys.exit()  
+    else:
+        return
+
+
+#Information message box :
+
+def messageShowinfo(title,text):
+        messagebox.showinfo(title,text)  
+
+
+#Question message boxes : 
+
+def messageAskquestion(title, text):
+    return messagebox.askquestion(title, text)
+
+
+def messageAskokcancel(title, text):
+    return messagebox.askokcancel(title, text)
+
+
+def messageAskretrycancel(title, text):
+    return messagebox.askretrycancel(title, text)
+
+
+def messageAskyesno(title, text):
+    return messagebox.askyesno(title, text)
+
+
+def messageAskyesnocancel(title, text):
+    return messagebox.askyesnocancel(title, text)
+
+
+#Warning message boxes :
+
+def messageShowwarning(title,text):
+    messagebox.showwarning(title, text)
+
+
+def messageShowerror(title,text):
+    messagebox.showerror(title, text)
+
+def askopenfilename(figure,path):
+    filename=filedialog.askopenfilename(initialdir = path,title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+    
+    try:
+        with open(filename,'r') as UseFile:
+            #print(UseFile.read())
+            figure.file = filename
+            print(figure.file)
+            return True
+    except:
         
+        
+        logging.warning(str(time.strftime('%Hh' '%M' ' %S')) + ' No file chosen')
+        messageShowwarning("Open Filename", "Warning, you must choose a file ")
+        return False
     

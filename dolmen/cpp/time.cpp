d0d3d2d72c1399a43d5302fe718d733b94e2c19b
Lubuntu <timothee.allegre@laposte.net>
2020-05-24 10:22:39 +0200

    time modifie, il rpend en compte la trame maintenant

--- dolmen/cpp/time.cpp
+++ dolmen/cpp/time.cpp
@@ -5,7 +5,7 @@ namespace dolmen
 {
   Time::Time (int id, std::string name):Sensor{id,name}{}
 
-  void Time::decoding(const std::string data)
+  /*void Time::decoding(const std::string data)
   {
     //actual time of the machine
     time_t tmm = time(0);
@@ -14,5 +14,35 @@ namespace dolmen
     double time = difftime (tmm, starting);
 
     insert("time", time);
+  }*/
+
+  void Time::decoding(const std::string data)
+  {
+  	//time will be coded in 2 hexa caracters, defining the number of seconds since the rocket startup
+    double time = 1.0;
+    std::string data_ = ""; 
+    int id = getID();
+    if (data.length() == 5 && data[4] == ';')
+    {
+      //we decode each character
+      if (true) {
+		data_ += data[2];
+      }
+      if (true) {
+        data_ += data[3];
+      }
+
+      time = std::stoi(data_, 0, 16);
+
+      std::cout << "\ntime: " << time << "\n";
+
+      //Temperature::temperature_ = temperature;
+      insert("time", time);
+    }
+    else
+    {
+      std::cout << "\nerror: bad data format";
+      insert("time_error", 0.0);
+    }
   }
 } /* dolmen */
